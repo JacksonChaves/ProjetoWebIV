863f3322fd911f49d6344cdb2386a3edf275f1d4
JacksonChaves <127264147+JacksonChaves@users.noreply.github.com>
2023-10-30 21:06:08 -0300

    Add files via upload

---
+++ Git/Back/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+ 
+class Movier(Base):
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key=True, index=True)
+    tmdb_id= Column(String)
+    title_id=Column(String)
+    #user_id
+    is_active = Column(Boolean, default=True)
+    
+
+class Favorito_movie(Base):
+    __tablename__ = "favoritos_movies"
+    tmdb_id = Column(String, primary_key=True, index=True)
+    # tmdb_id= Column(String)
+    # title_id=Column(String)
+    # is_active = Column(Boolean, default=True)
+    # user = relationship(User="user", back_populates="favorite_movies")
---
+++ Git/Front/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let menu = 1;
+	import Movie from "./Movie.svelte"
+	import Artista from "./Artista.svelte"
+	import User from "./User.svelte"
+	import Nav from "./Nav.svelte"
+	import UserList from "./UserList.svelte"
+	import Favoritos from "./Favoritos.svelte"
+
+</script>
+
+<Nav bind:menu/>
+
+<div class="card">
+	{#if menu === 1}
+		<Movie/>
+	{:else if menu === 2}
+		<Artista/>
+	{:else if menu === 3}
+		<User/>
+	{:else if menu === 4}
+		<UserList/>
+	{:else if menu === 5}
+		<Favoritos/>
+	{/if}
+</div>
